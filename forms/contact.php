bc09c7d65a93dae1caa9b19b443ea9ad16edde83
Agustina Luciana Gimenez <agus.luu@gmail.com>
2023-04-12 21:55:13 -0300

    mas contacto

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,45 @@
-<?php
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Documento sin título</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+
+<?php 
+error_reporting(0); 
+$nombre = $_POST['nombre']; 
+$correo= $_POST['correo']; 
+$comentario = $_POST['comentario']; 
+$header = 'From: ' . $correo  . " \r\n"; 
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
+$header .= "Mime-Version: 1.0 \r\n"; 
+$header .= "Content-Type: text/plain"; 
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n"; 
+$mensaje .= "Su e-mail es: " . $correo . " \r\n"; 
+$mensaje .= "con el mensaje: " . $comentario . " \r\n"; 
+$mensaje .= "Enviado el " . date('d/m/Y', time()); 
+
+$para = 'rodrigootero.pas@gmail.com'; 
+$asunto = 'mision'; 
+
+mail($para, $asunto, utf8_decode($mensaje), $header); 
+
+echo 'El mensaje ha sido enviado correctamente'; 
+
+?> 
+
+</body>
+</html>
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'rodrigootero.pas@gmail.com';
+ /* $receiving_email_address = 'rodrigootero.pas@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -25,11 +63,11 @@
     'password' => 'pass',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
-?>
+  echo $contact->send();*/
+
